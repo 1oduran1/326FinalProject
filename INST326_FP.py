bd187eb817a0eb07f3beb1572a61b83f5a5ca6d6
1oduran1 <oduran1@terpmail.umd.edu>
2024-04-08 17:26:03 -0400

    added Contact class and edited doc strings

--- INST326_FP.py
+++ INST326_FP.py
@@ -1,35 +1,60 @@
 """Contact Management application code
 Olivia Durán, Alison Wu, """
 
-class Phonebook:
-  """ An application for Contact Management and phone book logging
-  
+class Contact():
+    """A class for that represents a contact.
+
     Attributes:
-          name (str): the user's name.
-          
-          contacts (dict of str: Contact): dictionary where keys are the contact
-          name and values are their contact information
-          
-          book (list of contacts): list of contact objects that the user 
-          communicates with
-  """
-  def __init__ (self, name):
-    """ Initalize new Phonebook objects.
-
-    Args:
-        name (str): the user's name.
-
-    Side effects:
-        sets attributes name, contacts, and book
+        first_name (str): first name of the contact
+        last_name (str): last name of the contact
+        address (str): address of the contact
+        phone (str): phone number of the contact
+        email (str): email of the contact
     """
-    self.name = name
-    self.contacts = dict()
-    self.book = list()
+    def __init__(self, name, address, phone, email) -> None:
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.email = email
 
-  def add_contact(self, contact):
-    """ Add contact to contacts.
-        
+class Phonebook():
+    """ An application for Contact Management and phone book logging
+
+    Attributes:
+        contacts (dict of str: Contact): dictionary where keys are the contact
+        name and values are their contact information   
+    """
+    def __init__ (self):
+        """ Initalize new Phonebook objects.
         Args:
-            contact (Phonebook): Phonebook log of a contact to add
-    """
-    self.contacts[contact.name] = contact
+            None
+
+        Side effects:
+            sets contacts attribute
+        """
+        self.contacts = dict()
+
+    def add_contact(self, name, address, phone, email):
+        """ Add contact to contacts.
+            
+            Args:
+                name (str): name of contact
+                address (str): address of contact
+                phone (str): phone number of contact
+                email (str): email of contact
+                
+            Side effets: 
+                creates Contact object and adds contact information to
+            contacts dictionary
+        """
+        contact = Contact(name, address, phone, email)
+        self.contacts[contact.name] = (contact.address, contact.phone, contact.email)
+    
+    def show(self):
+        """ Shows full contact list.
+            
+            Args: None
+            
+            Side effects: None
+        """
+        return self.contacts
