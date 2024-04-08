928daa5ce86ba40479792251eb51eef6768b5c18
alison089 <148564064+alison089@users.noreply.github.com>
2024-04-08 12:23:25 -0400

    INSt326 docstrings

--- INST326_FP.py
+++ INST326_FP.py
@@ -1,11 +1,26 @@
 """Contact Management application code
 Olivia Durán, Alison Wu"""
 
-class Contact:
-  """
+class Phonebook:
+  """ An application for Contact Management and phone book logging
+  
     Attributes:
-
-    Returns:
-    
+          name (str): the user's name.
+          contacts (dict of str: Contact): the list of contacts that the users communicates with.
   """
-  def 
+  def __init__ (self, name):
+    """ Initalize new Phonebook objects.
+
+    Attributes:
+      Sets attributes to name, contacts, book .
+    """
+    self.name = name
+    self.contacts = dict()
+    self.book = list()
+
+  def __init__(self, contact):
+    """ Add contact to contacts.
+        
+        Args:
+            contact (Phonebook): Phonebook log of a contact to add
+    """
