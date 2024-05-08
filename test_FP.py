617077cfaaaa018b02e57c2d33e72e6c6c05ce60
1oduran1 <oduran1@terpmail.umd.edu>
2024-05-08 16:51:00 -0400

    working add_contact unit test

--- test_FP.py
+++ test_FP.py
@@ -2,23 +2,18 @@
 unit test for add_contact
 unit test for remove_contact
 unit test for update_contact
-unit test for clear_all_contacts"""
+unit test for clear_all_contacts
+unit test for main"""
 
 from INST326_FP import Phonebook
 import unittest
 
-#unit test for add_contact
+#unit test for main
 class TestPhonebook(unittest.TestCase):
     def setUp(self):
         self.phonebook = Phonebook()
-
+        
     def test_add_contact(self):
-        # Test adding a contact
         self.phonebook.add_contact("John Doe", "123 Main St", "555-1234", "john@example.com")
         self.assertTrue("John Doe" in self.phonebook.contacts)
         self.assertEqual(self.phonebook.contacts["John Doe"], ("123 Main St", "555-1234", "john@example.com"))
-
-        # Test adding another contact
-        self.phonebook.add_contact("Jane Smith", "456 Oak St", "555-5678", "jane@example.com")
-        self.assertTrue("Jane Smith" in self.phonebook.contacts)
-        self.assertEqual(self.phonebook.contacts["Jane Smith"], ("456 Oak St", "555-5678", "jane@example.com"))
